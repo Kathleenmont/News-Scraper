992f61e5b1f1c1a615ec840a312adf6c749cc570
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-06-14 16:28:00 -0400

    heroku atempt

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use(express.static("public"));
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
+// { useNewUrlParser: true }
 
 // Handlebars
 app.engine(
