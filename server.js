5c578acc91c1774a496d786f46d20cd756687dfb
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-06-15 09:50:45 -0400

    config

--- server.js
+++ server.js
@@ -25,8 +25,9 @@ app.use(express.static("public"));
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
+console.log(MONGODB_URI)
 mongoose.connect(MONGODB_URI);
+
 // { useNewUrlParser: true }
 
 // Handlebars
